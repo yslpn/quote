b26dcc221a5a4af2b6121598e6fd86c5a0f936d7
unknown <yaroslavslepukhin@gmail.com>
2018-03-31 21:14:20 +0300

    v4

--- main.js
+++ main.js
@@ -3,33 +3,28 @@
 const el = document.getElementsByTagName('blockquote'); // Quote
 const a = document.getElementsByTagName('p');       // Author
 const b = document.getElementsByTagName('button');    // Button
+const quoteJSON = function () {
+    return fetch('https://raw.githubusercontent.com/4skinSkywalker/Database-Quotes-JSON/master/quotes.json')
+        .then(x => x.json());
+}();
 
 let i = 0;
 
 b[0].addEventListener('click', function changeQ() {
-    fetch('https://raw.githubusercontent.com/4skinSkywalker/Database-Quotes-JSON/master/quotes.json')
-        .then(x => x.json())
-        .then(function (data) {
-            el[0].textContent = data[i].quoteText;
-            if (data[i].quoteAuthor) {
-                a[0].textContent = data[i].quoteAuthor;
-            } else {
-                a[0].textContent = '';
-            }
-        })
+    quoteJSON.then(function (data) {
+        el[0].textContent = data[i].quoteText;
+        a[0].textContent = data[i].quoteAuthor;
+    })
     i++;
 });
 
 a[0].addEventListener('click', function changeA() {
-    fetch('https://raw.githubusercontent.com/4skinSkywalker/Database-Quotes-JSON/master/quotes.json')
-        .then(x => x.json())
-        .then(function (data) {
-            for (let z = i + 1; z < data.length; z++) {
-                if (data[z].quoteAuthor === a[0].textContent) {
-                    a[0].textContent = data[z].quoteAuthor;
-                    el[0].textContent = data[z].quoteText;
-                    break;
-                }
+    quoteJSON.then(function (data) {
+        for (let z = i + 1; z < data.length; z++) {
+            if (data[z].quoteAuthor === a[0].textContent) {
+                a[0].textContent = data[z].quoteAuthor;
+                el[0].textContent = data[z].quoteText;
             }
-        })
+        }
+    })
 });
