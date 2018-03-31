fe6b1ed6f8682b28d496700dbd6200eef7170e34
unknown <yaroslavslepukhin@gmail.com>
2018-03-31 21:28:10 +0300

    fix

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const el = document.getElementsByTagName('blockquote'); // Quote
-const a = document.getElementsByTagName('p');       // Author
-const b = document.getElementsByTagName('button');    // Button
+const el = document.getElementsByTagName('blockquote');
+const a = document.getElementsByTagName('p');
+const b = document.getElementsByTagName('button');
 const quoteJSON = function () {
     return fetch('https://raw.githubusercontent.com/4skinSkywalker/Database-Quotes-JSON/master/quotes.json')
         .then(x => x.json());
@@ -10,7 +10,7 @@ const quoteJSON = function () {
 
 let i = 0;
 
-b[0].addEventListener('click', function changeQ() {
+b[0].addEventListener('click', function () {
     quoteJSON.then(function (data) {
         el[0].textContent = data[i].quoteText;
         a[0].textContent = data[i].quoteAuthor;
@@ -18,12 +18,12 @@ b[0].addEventListener('click', function changeQ() {
     i++;
 });
 
-a[0].addEventListener('click', function changeA() {
+a[0].addEventListener('click', function () {
     quoteJSON.then(function (data) {
         for (let z = i + 1; z < data.length; z++) {
             if (data[z].quoteAuthor === a[0].textContent) {
-                a[0].textContent = data[z].quoteAuthor;
                 el[0].textContent = data[z].quoteText;
+                a[0].textContent = data[z].quoteAuthor;
             }
         }
     })
