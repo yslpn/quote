747d0e1da014eb978f9567f79e7887b5f178e074
unknown <yaroslavslepukhin@gmail.com>
2018-04-02 14:41:21 +0300

    fix bug

--- main.js
+++ main.js
@@ -3,27 +3,30 @@
 const el = document.getElementsByTagName('blockquote');
 const a = document.getElementsByTagName('p');
 const b = document.getElementsByTagName('button');
-const quoteJSON = function () {
+const quoteJSON = (() => {
     return fetch('https://raw.githubusercontent.com/4skinSkywalker/Database-Quotes-JSON/master/quotes.json')
         .then(x => x.json());
-}();
+})();
 
-let i = 0;
-
-b[0].addEventListener('click', function () {
-    quoteJSON.then(function (data) {
-        el[0].textContent = data[i].quoteText;
-        a[0].textContent = data[i].quoteAuthor;
+let index = 0;
+b[0].addEventListener('click', () => {
+    quoteJSON.then((data) => {
+        el[0].textContent = data[index].quoteText;
+        a[0].textContent = data[index].quoteAuthor;
     })
-    i++;
+    index += 1;
+    forAuthorList = 0;
 });
 
-a[0].addEventListener('click', function () {
-    quoteJSON.then(function (data) {
-        for (let z = i + 1; z < data.length; z++) {
-            if (data[z].quoteAuthor === a[0].textContent) {
-                el[0].textContent = data[z].quoteText;
+let forAuthorList = 0;
+a[0].addEventListener('click', () => {
+    quoteJSON.then((data) => {
+        for (let z = forAuthorList; z < data.length; z++) {
+            if (data[z].quoteAuthor === a[0].textContent && data[z].quoteText !== el[0].textContent) {
                 a[0].textContent = data[z].quoteAuthor;
+                el[0].textContent = data[z].quoteText;
+                forAuthorList = z;
+                break;
             }
         }
     })
